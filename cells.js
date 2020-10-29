a56d77099acd17249441e8e464598cd9c012210d
MithunBhat <mithun.r.bhat99@gmail.com>
2020-10-29 09:39:53 +0530

    reffer this

--- cells.js
+++ cells.js
@@ -1,14 +1,45 @@
 class cells {
 
-    constructor(row, column, opened, flagged, mined, neighborMineCount) {
-        this.id = row + "" + column;
-        this.row = row;
-        this.column = column;
-        this.opened = opened;
-        this.flagged = flagged;
-        this.mined = mined;
-        this.neighborMineCount = neighborMineCount;
+    constructor(id, property) {
+        this.id = id;
+        this.property = property; //bomb or valid
+        this.createEle();
+    }
+
+    createEle = () => {
+        this.element = document.createAttribute('div');
+        this.element.setAttribute('id', this.id);
+        this.element.classList.add(this.property);
+        this.element.addEventListener('click', (e) => {
+            this.mining(this.element);
+        });
+        this.element.oncontexmenu = (e) => {
+            e.preventDefault();
+            addFlag(this.element);
+        }
+        return this.element;
     }
 
     
 }
+
+// class grid {
+//     i;
+//     difficulty = {
+//         'easy': [5, 10],
+//         'medium': [10, 25],
+//         'hard': [15, 45]
+//     }
+
+//     constructor(level) {
+//         [this.width, this.bombCount] = this.difficulty[level];
+//     };
+
+//     for (let i = 0; i < array.length; i++) {
+
+//     }
+
+//     ele = new cells()
+// }
+
+// let mit = new grid('easy');
