d67ecaf1f4981f332fbd5502c297206fd489cd02
MithunBhat <mithun.r.bhat99@gmail.com>
2020-10-29 14:30:14 +0530

    for now

--- cells.js
+++ cells.js
@@ -4,7 +4,6 @@ class cells {
     constructor(id, property) {
         this.id = id;
         this.property = property; //bomb or valid
-        this.createEle();
     }
 
     createEle = () => {
@@ -12,42 +11,92 @@ class cells {
         this.element.setAttribute('id', this.id);
         this.element.classList.add(this.property);
         this.element.addEventListener('click', () => {
-            this.mining(this.element);
+            this.mining();
         });
-        this.element.oncontexmenu = (e) => {
-            e.preventDefault();
-            this.addFlag(this.element);
+        this.element.oncontextmenu = (e) => {
+            e.preventDefault()
+            this.addFlag()
         }
-        return this.element;
+        return this.element;   
     }
 
-    mining = (ele) => {
+    mining = () => {
+        console.log('left click');
+        // if (isGameOver) return
+        if (this.element.classList.contains('checked') || this.element.classList.contains('flag')) return
+        if (this.element.classList.contains('bomb')) 
+		{
+			gameOver(this.element)
+		}
+        else 
+		{
+			let total = this.element.getAttribute('data')
+			if (total !=0) 
+			{
+				this.element.classList.add('checked')
+				if (total == 1) this.element.classList.add('one')
+				if (total == 2) this.element.classList.add('two')
+				if (total == 3) this.element.classList.add('three')
+				if (total == 4) this.element.classList.add('four')
+                this.element.innerHTML = total
+    			return
+            }
+        	checkSquare(this.element, this.id)
+		}
+		this.element.classList.add('checked')
 
     };
 
-    addFlag = (ele) => {
+    addFlag = () => {
+        console.log('right click');
+        // if (isGameOver) return
+		if (!this.element.classList.contains('checked') && (flags < bombAmount)) 
+		{
+            if (!square.classList.contains('flag')) 
+            {
+				square.classList.add('flag')
+                square.innerHTML = ' 🚩'
+                flags ++
+				flagsLeft.innerHTML = bombAmount- flags
+				checkForWin()
+			} 
+			else 
+			{
+				square.classList.remove('flag')
+					square.innerHTML = ''
+					flags --
+					flagsLeft.innerHTML = bombAmount- flags
+			}
+		}
 
     }
 
-    
 }
 
 class grid {
+    isGameOver = false;
     mit = document.querySelector('#mit');
     difficulty = {
-        'easy': [5, 10],
+        'easy': [2, 10], //gridWidth, bombAmount
         'medium': [10, 25],
         'hard': [15, 45]}
    
     constructor(level) {
-        [this.width, this.bombCount] = this.difficulty[level];
+        [this.width, this.bombAmount] = this.difficulty[level];
         this.start();
     }
 
     start = () => {
-        let shufellArray = ['valid', 'bomb', 'valid'];
-        this.square = new cells(this.width, shufellArray[0]);
-        this.mit.appendChild(this.square);
+        let shufellArray = ['valid', 'bomb', 'valid', 'valid'];
+
+        for (let i=0; i < (this.width*this.width); i++){
+            this.cellObj = new cells(i, shufellArray[i]);
+            var newCell = this.cellObj.createEle();
+            // newCell.addEventListener('click', () => {
+            //     this.cellObj.mining();
+            // })
+            this.mit.appendChild(newCell);
+        }
     }
 }
 
